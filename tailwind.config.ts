7943c3367aa6046e706e0f829fd863e7e53ee5ce
junk dog <ohmae@arubeh.com>
2024-01-19 14:03:38 +0900

    durationの設定

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-noto-sans-jp)'],
       },
+      transitionDuration: {
+        '400': '400ms',
+      },
     },
   },
   plugins: [],
