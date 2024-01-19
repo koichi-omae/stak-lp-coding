76d5488a8e3abefad207c01cc68b42729f6aaba8
junk dog <ohmae@arubeh.com>
2024-01-19 23:32:50 +0900

    男女比のPie Chart 作成

---
+++ src/components/Index/Data/GenderPieChart.tsx
@@ -0,0 +1,25 @@
+export default function GenderPieChartContainer() {
+  return (
+    <div className='relative mt-6 size-[272px]'>
+      <div className='gender-pie-chart'></div>
+      <div className='gender-text1 absolute'>
+        <p className='text-center text-white'>
+          <span className='font-condensed text-base'>男性</span>
+          <br />
+          <span className='-mt-3 inline-block font-condensed text-[52px]'>
+            55<span className='font-sans text-xl font-bold'>%</span>
+          </span>
+        </p>
+      </div>
+      <div className='gender-text2 absolute'>
+        <p className='text-center text-white'>
+          <span className='font-condensed text-base'>女性</span>
+          <br />
+          <span className='-mt-3 inline-block font-condensed text-[52px]'>
+            45<span className='font-sans text-xl font-bold'>%</span>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
