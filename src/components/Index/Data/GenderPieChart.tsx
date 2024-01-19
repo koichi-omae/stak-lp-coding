4a36e2426afc0a8a3e6154d9fb6f30ada9728ddb
junk dog <ohmae@arubeh.com>
2024-01-20 01:29:21 +0900

    男女比のPie Chartのsytle修正

--- src/components/Index/Data/GenderPieChart.tsx
+++ src/components/Index/Data/GenderPieChart.tsx
@@ -4,18 +4,18 @@ export default function GenderPieChartContainer() {
       <div className='gender-pie-chart'></div>
       <div className='gender-text1 absolute'>
         <p className='text-center text-white'>
-          <span className='font-condensed text-base'>男性</span>
+          <span className='font-condensed text-sm font-bold'>男性</span>
           <br />
-          <span className='-mt-3 inline-block font-condensed text-[52px]'>
+          <span className='-mt-2 inline-block font-condensed text-[52px]'>
             55<span className='font-sans text-xl font-bold'>%</span>
           </span>
         </p>
       </div>
       <div className='gender-text2 absolute'>
         <p className='text-center text-white'>
-          <span className='font-condensed text-base'>女性</span>
+          <span className='font-condensed text-sm font-bold'>女性</span>
           <br />
-          <span className='-mt-3 inline-block font-condensed text-[52px]'>
+          <span className='-mt-2 inline-block font-condensed text-[52px]'>
             45<span className='font-sans text-xl font-bold'>%</span>
           </span>
         </p>
