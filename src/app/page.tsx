fe3763c835f7f732e0be2b24dfb35a59be18cbed
junk dog <ohmae@arubeh.com>
2024-01-19 20:52:40 +0900

    メリットのコンポーネント作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import PageHeaderContainer from '@/components/Common/PageHeader';
 import FeatureContainer from '@/components/Index/Feature/Feature';
 import HeroContainer from '@/components/Index/Hero/Hero';
 import MenuContainer from '@/components/Index/Menu/Menu';
+import MeritContainer from '@/components/Index/Merit/Merit';
 
 export default function Home() {
   return (
     <>
       <PageHeaderContainer />
       <HeroContainer />
-      <MenuContainer />
-      <FeatureContainer />
+      <div className='bg-[#181818] pt-24 text-center text-white'>
+        <MenuContainer />
+        <FeatureContainer />
+        <MeritContainer />
+      </div>
     </>
   );
 }
---
+++ src/components/Index/Merit/Merit.tsx
@@ -0,0 +1,45 @@
+import MeritBoxPresentation, { MeritBoxProps } from './MeritBox';
+
+interface MeritPresentationProps {
+  title: string;
+  text: string;
+  boxes: MeritBoxProps[];
+}
+
+function MeritPresentation({ ...props }: MeritPresentationProps) {
+  return (
+    <div className='mt-24 text-center '>
+      <h2 className='text-3xl font-bold'>{props.title}</h2>
+      <p className='mt-5 text-base font-medium'>{props.text}</p>
+      {props.boxes.map((box, index) => {
+        return <MeritBoxPresentation key={index} {...box} />;
+      })}
+    </div>
+  );
+}
+
+export default function MeritContainer() {
+  const data: MeritPresentationProps = {
+    title: 'メリット',
+    text: 'ブランド強化から採用における差別化まで多面的なメリットを提供いたします',
+    boxes: [
+      {
+        title: 'ブランドイメージの向上',
+        text: '説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります',
+      },
+      {
+        title: '認知度拡大',
+        text: '説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります',
+      },
+      {
+        title: '商品およびサービスの販売促進',
+        text: '説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります',
+      },
+      {
+        title: '差別化による採用強化',
+        text: '説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文が入ります。説明文がはいります。説明文が入ります。説明文が入ります',
+      },
+    ],
+  };
+  return <MeritPresentation {...data} />;
+}
